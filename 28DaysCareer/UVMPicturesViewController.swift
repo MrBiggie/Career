b420fbe2de1d6e6ccb3beb51dbda066956f38619
xuelinzhao <zhaoxuelin1994@gmail.com>
2017-09-25 22:14:45 +1000

    Version 3.10 Unpack Vision Map

--- 28DaysCareer/UVMPicturesViewController.swift
+++ 28DaysCareer/UVMPicturesViewController.swift
@@ -16,9 +16,12 @@ class UVMPicturesViewController: UIViewController {
     @IBOutlet weak var imageThree: UIImageView!
     @IBOutlet weak var imageFour: UIImageView!
     
+    var receivedIdentifier: String = "SegueIdentifier"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(receivedIdentifier)
+        
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         
         let context = appDelegate.persistentContainer.viewContext
@@ -27,30 +30,47 @@ class UVMPicturesViewController: UIViewController {
         request.returnsObjectsAsFaults = false
         do
         {
-            //Read imageData
+            //Read data
             let results = try context.fetch(request)
             
             if results.count > 0{
                 for result in results as! [NSManagedObject]{
-                    print(result.description)
                     if let imageData1 = result.value(forKey: "image1") as? Data{
                         let image1 = UIImage(data: imageData1)
                         self.imageOne.image = image1
+                        imageOne.isUserInteractionEnabled = true
+                        let tapGR = UITapGestureRecognizer(target: self, action: #selector(tapHandlerOne))
+                        tapGR.numberOfTapsRequired = 1
+                        imageOne.addGestureRecognizer(tapGR)
                     }
                     
                     if let imageData2 = result.value(forKey: "image2") as? Data{
                         let image2 = UIImage(data: imageData2)
                         self.imageTwo.image = image2
+                        imageTwo.isUserInteractionEnabled = true
+                        let tapGR = UITapGestureRecognizer(target: self, action: #selector(tapHandlerTwo))
+                        tapGR.numberOfTapsRequired = 1
+                        imageTwo.addGestureRecognizer(tapGR)
+                        
                     }
                     
                     if let imageData3 = result.value(forKey: "image3") as? Data{
                         let image3 = UIImage(data: imageData3)
                         self.imageThree.image = image3
+                        imageThree.isUserInteractionEnabled = true
+                        let tapGR = UITapGestureRecognizer(target: self, action: #selector(tapHandlerThree))
+                        tapGR.numberOfTapsRequired = 1
+                        imageThree.addGestureRecognizer(tapGR)
                     }
                     
                     if let imageData4 = result.value(forKey: "image4") as? Data{
                         let image4 = UIImage(data: imageData4)
                         self.imageFour.image = image4
+                        imageFour.isUserInteractionEnabled = true
+                        let tapGR = UITapGestureRecognizer(target: self, action: #selector(tapHandlerFour))
+                        tapGR.numberOfTapsRequired = 1
+                        imageFour.addGestureRecognizer(tapGR)
+                        
                     }
                 }
             }
@@ -59,9 +79,44 @@ class UVMPicturesViewController: UIViewController {
         {
             //PROCESS ERROR
         }
+        
     }
-
     
+    //tapHandlerOne
+    func tapHandlerOne(){
+        print("tapHandlerOne")
+        let VC = self.storyboard?.instantiateViewController(withIdentifier: receivedIdentifier) as! UVMSelectPictureViewController
+        VC.receivedImage = "ImageOne"
+        VC.buttonVisiable = false
+        self.present(VC, animated: true, completion: nil)
+    }
+    
+    //tapHandlerTwo
+    func tapHandlerTwo(){
+        print("tapHandlerTwo")
+        let VC = self.storyboard?.instantiateViewController(withIdentifier: receivedIdentifier) as! UVMSelectPictureViewController
+        VC.receivedImage = "ImageTwo"
+        VC.buttonVisiable = false
+        self.present(VC, animated: true, completion: nil)
+    }
+    
+    //tapHandlerThree
+    func tapHandlerThree(){
+        print("tapHandlerThree")
+        let VC = self.storyboard?.instantiateViewController(withIdentifier: receivedIdentifier) as! UVMSelectPictureViewController
+        VC.receivedImage = "ImageThree"
+        VC.buttonVisiable = false
+        self.present(VC, animated: true, completion: nil)
+    }
+    
+    //tapHandlerFour
+    func tapHandlerFour(){
+        print("tapHandlerFour")
+        let VC = self.storyboard?.instantiateViewController(withIdentifier: receivedIdentifier) as! UVMSelectPictureViewController
+        VC.receivedImage = "ImageFour"
+        VC.buttonVisiable = false
+        self.present(VC, animated: true, completion: nil)
+    }
     
     
     override func didReceiveMemoryWarning() {
@@ -69,5 +124,5 @@ class UVMPicturesViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
 }
--- 28DaysCareer/UVMSelectPictureViewController.swift
+++ 28DaysCareer/UVMSelectPictureViewController.swift
@@ -7,35 +7,89 @@
 //
 
 import UIKit
+import CoreData
 
 class UVMSelectPictureViewController: UIViewController {
 
-    @IBAction func HappySelectButton(_ sender: UIButton) {
-        
-           }
+    var receivedImage: String = "ImageZero"
+    var buttonVisiable: Bool =  true
     
-    @IBAction func InspireSelectButton(_ sender: UIButton) {
-        
-    }
+    @IBOutlet weak var PickPicButton: UIButton!
     
     
-    @IBAction func ChallengeSelectButton(_ sender: UIButton) {
-        
-    }
-    
+    @IBOutlet weak var SelectImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        if buttonVisiable == false{
+            PickPicButton.setTitle(" ", for: .normal)
+        }
+        print(receivedImage)
         // Do any additional setup after loading the view.
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Image")
+        request.returnsObjectsAsFaults = false
+        do
+        {
+            //Read data
+            let results = try context.fetch(request)
+            
+            if results.count > 0{
+                for result in results as! [NSManagedObject]{
+                    
+                    if(receivedImage == "ImageOne"){
+                        if let imageData1 = result.value(forKey: "image1") as? Data{
+                            let image1 = UIImage(data: imageData1)
+                            SelectImageView.image = image1
+                        }
+                    }
+                    
+                    if(receivedImage == "ImageTwo"){
+                        if let imageData2 = result.value(forKey: "image2") as? Data{
+                            let image2 = UIImage(data: imageData2)
+                            SelectImageView.image = image2
+                            
+                        }
+                    }
+                    
+                    if(receivedImage == "ImageThree"){
+                        if let imageData3 = result.value(forKey: "image3") as? Data{
+                            let image3 = UIImage(data: imageData3)
+                            SelectImageView.image = image3
+                        }
+                    }
+                    
+                    if(receivedImage == "ImageFour"){
+                        if let imageData4 = result.value(forKey: "image4") as? Data{
+                            let image4 = UIImage(data: imageData4)
+                            SelectImageView.image = image4
+                        }
+                    }
+                }
+            }
+        }
+        catch
+        {
+            //PROCESS ERROR
+        }
+        
+        
     }
-
-    
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let secondVC: UVMPicturesViewController = segue.destination as? UVMPicturesViewController{
+            secondVC.receivedIdentifier = segue.identifier!
+        }
+    }
+
     
 }
