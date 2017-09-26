7127e6b8f023ba73af55190bbfbc01ef1efec080
xuelinzhao <zhaoxuelin1994@gmail.com>
2017-09-26 16:42:00 +1000

    Version 3.3.1 Update Relaxation Audio

--- 28DaysCareer/WLBIntroductionViewController.swift
+++ 28DaysCareer/WLBIntroductionViewController.swift
@@ -14,12 +14,25 @@ class WLBIntroductionViewController: UIViewController {
     
     var player: AVAudioPlayer?
     
+    @IBOutlet weak var IntroductionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let url = Bundle.main.url(forResource: "wlbintroduction", withExtension: "m4a")!
+        // Default URL
+        var URL: URL = Bundle.main.url(forResource: "Four_Seasons_Autumn_Movt_1", withExtension: "mp3")!
+        
+        // Choose Audio URL accordiong to the title
+        if(IntroductionLabel.text == "Work-life Balance"){
+            URL = Bundle.main.url(forResource: "wlbintroduction", withExtension: "m4a")!
+        }
+        
+        if(IntroductionLabel.text == "Relaxation"){
+            URL = Bundle.main.url(forResource: "Four_Seasons_Autumn_Movt_1", withExtension: "mp3")!
+        }
+        
         do {
-            player = try AVAudioPlayer(contentsOf: url)
+            player = try AVAudioPlayer(contentsOf: URL)
             player?.prepareToPlay()
         } catch let error {
             print(error.localizedDescription)
@@ -46,16 +59,5 @@ class WLBIntroductionViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
     
 }
